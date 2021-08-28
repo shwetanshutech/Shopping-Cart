dfd84bbea6d2ce984d02e88a7d58cf909deb8baf
shwetanshutech <shwetanshu2000@gmail.com>
2021-08-28 20:07:43 +0530

    Item Class added

--- cart_main.cpp
+++ cart_main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 signed main(){
     Product p(1,"apple",26);
     cout<<p.getDisplayName();
-
+    Item fruit(p,3);
+    cout<<fruit.getItemInfo();
 	return 0;
 }
--- datamodel.h
+++ datamodel.h
@@ -1,6 +1,10 @@
 //Product ,Item, Cart
 #include<string>
 using namespace std;
+
+//forward declaration
+class Item; 
+class Cart;
 class Product{
   int id;
   string name;
@@ -14,14 +18,25 @@ class Product{
    }
 
    string getDisplayName(){
-   	return name+" :Rs "+ to_string(price)<<endl;
+   	return name+" : Rs "+ to_string(price)+'\n';
    }
-
+  friend class Item;
 };
 
 
 class Item{
+     Product product;
+     int quantity;
+ public:
+    //Constructor using a intiliasation list
+     Item(Product p, int q):product(p),quantity(q){}
+    int getPrice(){
+        return quantity*product.price;
+    }
 
+    string getItemInfo(){
+        return to_string(quantity)+ " x "+product.name+" = "+"Rs "+to_string(quantity*product.price);
+    }
 };
 
 class Cart{
